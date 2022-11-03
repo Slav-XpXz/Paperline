ed5952917d62b5f13785a4f6a5e304ba5fdfbbd6
Slav XpXz <76703277+Slav-XpXz@users.noreply.github.com>
2022-11-03 19:03:53 -0400

    Added funny message

--- src/alpha/core.c
+++ src/alpha/core.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
            }
            fputs("alert('Paperline');", fpp);
            fclose(fpp);
-        
+           printf("Sucess! \n");
         }
     }
 

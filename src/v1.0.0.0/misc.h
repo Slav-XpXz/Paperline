5e58693198eda15ba7ee4ce79122f8da280a596c
Slav-XpXz <Nightmareboi000@gmail.com>
2022-11-04 17:08:20 -0400

    v.1.0.0.0 is out!

---
+++ src/v1.0.0.0/misc.h
@@ -0,0 +1,8 @@
+void help(void)
+{
+    printf("Paperline webapp - Creates a small web application");
+    printf("Paperline (Operation) - you aren't not that dumb");
+    printf("Paperline ss - takes a screenshot");
+}
+
+

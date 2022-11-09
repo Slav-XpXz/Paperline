29c144f206aa89e7032c039859fe730f55bf4265
Slav XpXz <76703277+Slav-XpXz@users.noreply.github.com>
2022-11-09 14:04:32 -0500

    forgot to add newline

--- src/v1.0.0.0/misc.h
+++ src/v1.0.0.0/misc.h
@@ -1,8 +1,8 @@
 void help(void)
 {
-    printf("Paperline webapp - Creates a small web application");
-    printf("Paperline (Operation) - you aren't not that dumb");
-    printf("Paperline ss - takes a screenshot");
+    printf("Paperline webapp - Creates a small web application \n");
+    printf("Paperline (Operation) - you aren't not that dumb \n");
+    printf("Paperline ss - takes a screenshot \n");
 }
 
 
